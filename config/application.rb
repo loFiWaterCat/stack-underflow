ac05824554e39f8c5af18017386cbf19aac9fbc0
alan <alan001tk@gmail.com>
2022-09-06 13:59:20 -0700

    Please work now

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module StackUnderflowBackend
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Please
+    # Please work
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
