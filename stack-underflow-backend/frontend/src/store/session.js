0968506b543520527d83e8f76dfc51965460dbd5
alan <alan001tk@gmail.com>
2022-09-06 10:21:53 -0700

    Finish auth me phase 2

--- stack-underflow-backend/frontend/src/store/session.js
+++ stack-underflow-backend/frontend/src/store/session.js
@@ -40,6 +40,23 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  storeCurrentUser(data.user);
+  dispatch(setCurrentUser(data.user));
+  return response;
+};
+
+
 export const restoreSession = () => async dispatch => {
   const response = await csrfFetch("/api/session");
   storeCSRFToken(response);
