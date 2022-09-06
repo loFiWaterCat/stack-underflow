ae3d3a658666971d0ab31d50ecc35d71da0eface
alan <alan001tk@gmail.com>
2022-09-06 10:46:25 -0700

    Finish auth me front end phase 3

--- stack-underflow-backend/frontend/src/store/session.js
+++ stack-underflow-backend/frontend/src/store/session.js
@@ -56,6 +56,14 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE"
+  });
+  storeCurrentUser(null);
+  dispatch(removeCurrentUser());
+  return response;
+};
 
 export const restoreSession = () => async dispatch => {
   const response = await csrfFetch("/api/session");
